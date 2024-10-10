3e4399c7c68653c425445f3ea5a7a5918c975535
Jose-VanHelsing <alejandrovanegas14@hotmail.com>
2024-10-10 07:01:55 -0500

    Add files via upload

---
+++ Cartagena/Formulario.js
@@ -0,0 +1,10 @@
+//declarando variables
+const contenedor_formulario = document.querySelector('contenedor_formulario');
+const Contactoform = document.querySelector('Contactoform');
+
+
+function enviar(){
+    
+}
+
+
